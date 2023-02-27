3213351ec7cac67aed80f5f12acc33faee802c27
Johanna Florange <florangejohanna@gmail.com>
2023-02-27 15:14:49 +0100

    adding function to handle change on form

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import {FC} from 'react';
+import { FC, ChangeEvent, useState } from 'react';
 import './App.css';
 
 const App: FC = () => {
+
+  const [tasks, setTasks] = useState<string>("");
+  const [deadLine, setDeadLine] = useState<number>(0);
+  const [todoList, setTodoList] = useState([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) =>{
+    if (event.target.name === 'task') {
+      setTasks(event.target.value);
+    } else {
+      setDeadLine(Number(event.target.value));
+    }
+  }
+
   return (
     <div className="App">
       <header className="header">
         <div className="container">
-          <input type="text" placeholder='Task...' />
-          <input type="number" placeholder='Time...' />
+          <input type="text" placeholder='Task...' name='task' onChange={handleChange} />
+          <input type="number" placeholder='Deadline (in days)...' name='deadline' onChange={handleChange}
+           />
         </div>
         <button>Add Task</button>
       </header>
