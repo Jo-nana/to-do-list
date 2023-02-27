e4ee81b9fa0aca5c6d645ad3a9510fb5de72403e
Johanna Florange <florangejohanna@gmail.com>
2023-02-27 16:22:22 +0100

    adding "adding task" function and delete button + display list of task

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,62 @@
 import { FC, ChangeEvent, useState } from 'react';
 import './App.css';
+import TodoTasks from './components/todotasks';
+import { Task } from './Interfaces'
 
 const App: FC = () => {
-
-  const [tasks, setTasks] = useState<string>("");
+  // state variables
+  const [task, setTask] = useState<string>("");
   const [deadLine, setDeadLine] = useState<number>(0);
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState<Task[]>([]);
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) =>{
+  // function to handle change in input fields
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void =>{
     if (event.target.name === 'task') {
-      setTasks(event.target.value);
+      setTask(event.target.value);
     } else {
       setDeadLine(Number(event.target.value));
     }
   }
 
+  // function to add task to todoList array
+  const addTask = (): void => {
+    const newTask = { taskName: task, taskDeadline: deadLine };
+    setTodoList([...todoList, newTask]);
+    setTask("");
+    setDeadLine(0);
+  }
+
+  const completeTask = (taskNameToDelete: string): void => {
+    setTodoList(todoList.filter((task) => {
+      return task.taskName !== taskNameToDelete; // keep all tasks that don't match the taskNameToDelete
+    }))
+  }
   return (
     <div className="App">
       <header className="header">
         <div className="container">
-          <input type="text" placeholder='Task...' name='task' onChange={handleChange} />
-          <input type="number" placeholder='Deadline (in days)...' name='deadline' onChange={handleChange}
-           />
+          <input
+            type="text"
+            placeholder='Task...'
+            name='task'
+            value={task}
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            placeholder='Deadline (in days)...'
+            name='deadline'
+            value={deadLine}
+            onChange={handleChange}
+          />
         </div>
-        <button>Add Task</button>
+        <button onClick={addTask}>Add Task</button>
       </header>
-      <div className="list"></div>
+      <div className="list">
+        {todoList.map((task: Task, key: number)=> {
+          return <TodoTasks key={key} task={task} completeTask={completeTask}/>;
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/components/todotasks.tsx
@@ -0,0 +1,22 @@
+import { Task } from "../Interfaces";
+
+// setting up the interface for the props, with Task as the type
+interface Props {
+  task: Task;
+  completeTask: (taskNameToDelete: string) => void;
+}
+
+// displaying the task into a list
+const TodoTasks = ({ task, completeTask }: Props) => {
+  return (
+    <div className='task'>
+      <div className='content'>
+        <span>{task.taskName}</span>
+        <span>{task.taskDeadline} days</span>
+      </div>
+      <button className='btn-delete' onClick={() => completeTask(task.taskName)}>X</button>
+    </div>
+  )
+};
+
+export default TodoTasks;
