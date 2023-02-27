894616026978d2e1f85f3563b1a42ed2edf1af77
Johanna Florange <florangejohanna@gmail.com>
2023-02-27 14:26:11 +0100

    simple form for the task added

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,14 @@ import './App.css';
 const App: FC = () => {
   return (
     <div className="App">
-
+      <header className="header">
+        <div className="container">
+          <input type="text" placeholder='Task...' />
+          <input type="number" placeholder='Time...' />
+        </div>
+        <button>Add Task</button>
+      </header>
+      <div className="list"></div>
     </div>
   );
 }
