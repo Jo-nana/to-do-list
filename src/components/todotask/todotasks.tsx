dedb1c139e8ece64e2ff9286de884e72ddbe3df0
Johanna Florange <florangejohanna@gmail.com>
2023-02-28 15:57:20 +0100

    adding a checbox to crossed out the taks that has been completed

--- src/components/todotask/todotasks.tsx
+++ src/components/todotask/todotasks.tsx
@@ -1,5 +1,5 @@
 import { Task } from "../../Interfaces";
-
+import { useState } from "react";
 import './todotasks.scss';
 
 // setting up the interface for the props, with Task as the type
@@ -10,16 +10,30 @@ interface Props {
 
 // displaying the task into a list
 const TodoTasks = ({ task, completeTask }: Props) => {
+  const [isComplete, setIsComplete] = useState(false);
+
+  const toggleComplete = () => {
+    setIsComplete(!isComplete);
+  }
+
+  const taskContentClass = isComplete ? 'task-content crossed-out' : '';
+
   return (
     <div className='task-container'>
-      <div className='task-content'>
+      <div className={`${taskContentClass} task-content`}>
         <h1>{task.taskName}</h1>
         <p>{task.taskDescription}</p>
       </div>
       <div className='task-content-bottom'>
-        <p>{task.taskDeadline} days</p>
-        <div className='trash-icon' onClick={() => completeTask(task.taskName)}>
-          <i className="fa-solid fa-trash"></i>
+        <p className={taskContentClass}>{task.taskDeadline} days</p>
+        <div className='checkbox-bottom'>
+          <div className='task-done'>
+            <input type="checkbox" onChange={toggleComplete} checked={isComplete} />
+            <label>Done</label>
+          </div>
+          <div className='trash-icon' onClick={() => completeTask(task.taskName)}>
+            <i className="fa-solid fa-trash"></i>
+          </div>
         </div>
       </div>
     </div>
